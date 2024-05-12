89990d05cd32b2a3bc92e2e13aeb74bda1334f1e
Naitik2408 <naitikkumar2408@gmail.com>
2024-05-12 17:39:58 +0530

    soved some important issue

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -85,7 +85,7 @@ function Home() {
                         return 
                     }
                     setShorUrl("block")
-                    setShortenedURL(`https://www.n2r.store/${data.id}`);
+                    setShortenedURL(`${conf.api_url}/${data.id}`);
                 } else {
                     console.error('Error:', response.statusText);
                 }
